d5de49369df401d3b3d8044780a2fa8a564cb0cb
Toluwalashe <tee1alive@yahoo.com>
2021-01-29 22:08:55 +0100

    new repo

---
+++ src/app.controller.ts
@@ -0,0 +1,80 @@
+import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @Get('/')
+    getData() {
+        return this.appService.getData();
+    }
+
+    @Post('/validate-rule')
+    validate(@Body() data){
+      const response = {
+        message: "rule should be an object.",
+        status: "error",
+        data: "null"
+      }
+
+      const response0 = {
+        message: `Invalid JSON payload passed.`,
+        status: "error",
+        data: "null"
+        }
+
+      const response1 = {
+        message: "rule is required.",
+        status: "error",
+        data: "null"
+      }
+
+      const response2 = {
+        message: "data is required.",
+        status: "error",
+        data: "null"
+      }
+
+      const response3 = {
+        message: `field ${data.rule.field} is missing from data.`,
+        status: "error",
+        data: "null"
+      }
+
+      // if (data.rule !== JSON) {
+      //   return response;
+      // }
+      // else if (data.data !== JSON) {
+      //   return response;
+      // }
+      
+      if (data.rule !== null && typeof data.rule !== 'object') {
+        throw new HttpException(response, HttpStatus.BAD_REQUEST);
+      }
+
+      if(!data.rule.field || !data.rule.condition || !data.rule.condition_value){
+        throw new HttpException(response0, HttpStatus.BAD_REQUEST);
+      }
+      if (!data.data[data.rule.field]) {
+        throw new HttpException(response3, HttpStatus.BAD_REQUEST);
+      }
+
+      if (!data.rule && !data.data) {
+        throw new HttpException(response0, HttpStatus.BAD_REQUEST);
+      }
+      else if (!data.rule) {
+        throw new HttpException(response1, HttpStatus.BAD_REQUEST);
+      }
+      else if (!data.data) {
+        throw new HttpException(response2, HttpStatus.BAD_REQUEST);
+      }
+      return this.appService.validate(data);
+    }  
+    
+}
---
+++ src/app.service.ts
@@ -0,0 +1,93 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  response = 
+    {
+    message : "My Rule-Validation API",
+    status : "success",
+    data :
+    {
+        name: "Dada Toluwalase",
+        github : "@lashe",
+        email : "tee1alive@yahoo.com",
+        mobile : "07065518784"
+    }
+    
+  };
+    
+
+   getData() {
+       return this.response;
+   }
+
+    validate(req) {
+        const dataField = req.rule.field;
+        const rule = {
+            condition: req.rule.condition,
+            condition_value: req.rule.condition_value
+        }
+        const data = {
+            field: req.data[dataField]
+        }
+        console.log(data.field);
+
+        const response1 = {
+            message: `field ${dataField} successfully validated.`,
+            status: "success",
+            data: {
+                validation: {
+                    error: false,
+                    field: dataField,
+                    field_value: data.field,
+                    condition: rule.condition,
+                    condition_value: rule.condition_value
+                }
+            }
+        }
+        const response2 = {
+            message: `field ${dataField} validation failed.`,
+            status: "error",
+            data: {
+                validation: {
+                    error: true,
+                    field: dataField,
+                    field_value: data.field,
+                    condition: rule.condition,
+                    condition_value: rule.condition_value
+                }
+            }
+        }
+
+        if (rule.condition === 'eq') {
+            if (data.field === rule.condition_value) {
+                return response1;
+            }
+            throw new HttpException(response2, HttpStatus.BAD_REQUEST);
+        } else if (rule.condition === 'neq') {
+            if (data.field !== rule.condition_value) {
+                return response1;
+            }
+            throw new HttpException(response2, HttpStatus.BAD_REQUEST);
+        } else if (rule.condition === 'gt') {
+            if (data.field > rule.condition_value) {
+                return response1;
+            }
+            throw new HttpException(response2, HttpStatus.BAD_REQUEST);
+        } else if (rule.condition === 'gte') {
+            if (data.field >= rule.condition_value) {
+                return response1;
+            }
+            throw new HttpException(response2, HttpStatus.BAD_REQUEST);
+        } else if (rule.condition === 'contains') {
+            if (data.field.includes(rule.condition_value) === true) {
+                return response1;
+            }
+            throw new HttpException(response2, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
