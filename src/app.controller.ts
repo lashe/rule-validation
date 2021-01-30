9fdaf3ac02b5974774e6d54c484814a7cc8fecdf
Toluwalashe <tee1alive@yahoo.com>
2021-01-30 09:44:32 +0100

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   // @Get()
   // getHello(): string {
@@ -11,63 +11,86 @@ export class AppController {
   // }
 
   @Get('/')
-    getData() {
-        return this.appService.getData();
+  getData() {
+    return this.appService.getData();
+  }
+
+  @Post('/validate-rule')
+  validate(@Body() data) {
+    const response = {
+      message: "rule should be an object.",
+      status: "error",
+      data: "null"
     }
 
-    @Post('/validate-rule')
-    validate(@Body() data){
-      const response = {
-        message: "rule should be an object.",
-        status: "error",
-        data: "null"
-      }
+    const response0 = {
+      message: `Invalid JSON payload passed.`,
+      status: "error",
+      data: "null"
+    }
 
-      const response0 = {
-        message: `Invalid JSON payload passed.`,
-        status: "error",
-        data: "null"
-        }
+    const response1 = {
+      message: "rule is required.",
+      status: "error",
+      data: "null"
+    }
 
-      const response1 = {
-        message: "rule is required.",
-        status: "error",
-        data: "null"
-      }
+    const response2 = {
+      message: "data is required.",
+      status: "error",
+      data: "null"
+    }
 
-      const response2 = {
-        message: "data is required.",
-        status: "error",
-        data: "null"
-      }
+    const response3 = {
+      message: `field ${data.rule.field} is missing from data.`,
+      status: "error",
+      data: "null"
+    }
 
-      const response3 = {
-        message: `field ${data.rule.field} is missing from data.`,
-        status: "error",
-        data: "null"
-      }
+    const response4 = {
+      message: "field is required.",
+      status: "error",
+      data: "null"
+    }
 
-      if (data.rule !== null && typeof data.rule !== 'object') {
-        throw new HttpException(response, HttpStatus.BAD_REQUEST);
-      }
+    const response5 = {
+      message: "condition is required.",
+      status: "error",
+      data: "null"
+    }
 
-      if(!data.rule.field || !data.rule.condition || !data.rule.condition_value){
-        throw new HttpException(response0, HttpStatus.BAD_REQUEST);
-      }
-      if (!data.data[data.rule.field]) {
-        throw new HttpException(response3, HttpStatus.BAD_REQUEST);
-      }
+    const response6 = {
+      message: "condition_value is required.",
+      status: "error",
+      data: "null"
+    }
 
-      if (!data.rule && !data.data) {
-        throw new HttpException(response0, HttpStatus.BAD_REQUEST);
-      }
-      else if (!data.rule) {
-        throw new HttpException(response1, HttpStatus.BAD_REQUEST);
-      }
-      else if (!data.data) {
-        throw new HttpException(response2, HttpStatus.BAD_REQUEST);
-      }
-      return this.appService.validate(data);
-    }  
-    
+    if (data.rule !== null && typeof data.rule !== 'object') {
+      throw new HttpException(response, HttpStatus.BAD_REQUEST);
+    }
+    if (!data.rule.field) {
+      throw new HttpException(response4, HttpStatus.BAD_REQUEST);
+    }
+    if (!data.rule.condition) {
+      throw new HttpException(response5, HttpStatus.BAD_REQUEST);
+    }
+    if (!data.rule.condition_value) {
+      throw new HttpException(response6, HttpStatus.BAD_REQUEST);
+    }
+    if (!data.data[data.rule.field]) {
+      throw new HttpException(response3, HttpStatus.BAD_REQUEST);
+    }
+
+    if (!data.rule && !data.data) {
+      throw new HttpException(response0, HttpStatus.BAD_REQUEST);
+    }
+    else if (!data.rule) {
+      throw new HttpException(response1, HttpStatus.BAD_REQUEST);
+    }
+    else if (!data.data) {
+      throw new HttpException(response2, HttpStatus.BAD_REQUEST);
+    }
+    return this.appService.validate(data);
+  }
+
 }
